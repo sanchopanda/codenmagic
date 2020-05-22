cad7bf2c20fe440def1619a22b0759658cf755aa
sanchopanda <ivanovaa1992@gmail.com>
2020-05-22 15:56:26 +0300

    module6

--- js/data.js
+++ js/data.js
@@ -4,6 +4,9 @@
     var similarWizardTemplate = document.querySelector('#similar-wizard-template')
         .content
         .querySelector('.setup-similar-item');
+
+    //МОК генерация
+    /*
     var wizardsFirstName = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктория", "Юлия", "Лолита", "Вашингтон"];
     var wizardsLastName = ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
 
@@ -25,20 +28,47 @@
         }
         return wizards;
     };
-
+*/
     function createWizard(wizard) {
         var wizardElement = similarWizardTemplate.cloneNode(true);
         wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-        wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-        wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+        wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+        wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
         return wizardElement;
     };
 
-    var wizards = randomWizards(4);
+    //загрузка волшебников
+    function wizardsGetSucces(wizards) {
+        var fragment = document.createDocumentFragment();
+        for (var i = 4; i < 9; i++) {
+            fragment.appendChild(createWizard(wizards[i]));
+        }
+        similarListElement.appendChild(fragment);
+    };
 
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wizards.length; i++) {
-        fragment.appendChild(createWizard(wizards[i]));
-    }
-    similarListElement.appendChild(fragment);
+    function GetError(errorMessage) {
+        var node = document.createElement('div');
+        node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+        node.style.position = 'absolute';
+        node.style.left = 0;
+        node.style.right = 0;
+        node.style.fontSize = '30px';
+        node.textContent = errorMessage;
+        document.body.insertAdjacentElement('afterbegin', node);
+    };
+
+    backend.load(wizardsGetSucces, GetError);
+
+
+    //отправка формы
+    function formPostSucces() {
+        document.querySelector('.setup').classList.add('hidden');
+    };
+
+    var form = document.querySelector('.setup-wizard-form');
+
+    form.addEventListener('submit', function (evt) {
+        backend.save(new FormData(form), formPostSucces, GetError);
+        evt.preventDefault();
+    });
 })();
